768fb38ac4f986d162a8de167b19c4b1828bef69
Konststep <Rain1-1@mail.ru>
2026-04-25 12:56:59 +0300

    Add files via upload

---
+++ zadanie1.5.3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <clocale>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a[3][6]{};
+
+    int i, j, min, max, min_i, min_j, max_i, max_j;
+
+    cout << "Массив: " << endl;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            a[i][j] = rand() & 100;
+            cout << a[i][j] << "\t";
+        }
+        cout << endl;
+    }
+        min = a[0][0];
+        max = a[0][0];
+
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 6; j++)
+            {
+                if (min > a[i][j])
+                {
+                    min = a[i][j];
+                    min_i = i + 1;
+                    min_j = j + 1;
+                }
+               
+                if (max < a[i][j])
+                {
+                    max = a[i][j];
+                    max_i = i + 1;
+                    max_j = j + 1;
+                }
+                
+            }
+        }
+    
+        cout << endl;
+        cout << "Индекс минимального элемента: [" << min_i << "] [" << min_j << "]" << endl;
+        cout << "Индекс максимальный элемента: [" << max_i << "] [" << max_j << "]" << endl;
+        return EXIT_SUCCESS;
+    }
